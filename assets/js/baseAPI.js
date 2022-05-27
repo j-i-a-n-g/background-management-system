c7b1639c5d3f8f436290b684e3aa20bbbc6f57d0
jiang <1280291927@qq.com>
2022-05-27 10:26:21 +0800

    完成了主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,4 +2,21 @@
 // 在发起真正的ajax请求之前，统一拼接请求的根路径
 $.ajaxPrefilter(function(option) {
     option.url = 'http://www.liulongbin.top:3007' + option.url
+
+    // 统一为有权限的接口，设置headers请求头
+    if (option.url.indexOf("/my/") !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ""
+        }
+    };
+
+    option.complete = function(res) {
+        if (res.responseJSON.status == 1 && res.responseJSON.message == "身份认证失败！") {
+            // 清空token;
+            localStorage.removeItem('token');
+
+            // 页面跳转到登录页面
+            location.href = '/login.html';
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+window.addEventListener('load', function() {
+    // 获取用户的基本信息
+    $.ajax({
+        url: '/my/userinfo',
+        method: 'GET',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ""
+        // },
+        success: function(res) {
+            if (res.status !== -0) {
+                return layui.layer.msg(res.message)
+            }
+            // 调用函数渲染头像
+            renderAcvatar(res.data)
+        }
+
+    })
+    var layer = layui.layer;
+    // 点击按钮，实现退出功能
+    $('#btnLogout').on('click', function() {
+        // 提示用户是否确认退出
+        layer.confirm('是否退出登录', { icon: 3, title: '提示' }, function(index) {
+            // 1.清空本地存储中的token
+            localStorage.removeItem('token');
+            // 2.重新跳转到登录页面
+            location.href = '/login.html';
+            // 关闭询问框
+            layer.close(index);
+
+        })
+    })
+
+    function renderAcvatar(user) {
+        var name = user.nickname || user.username;
+
+        $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+
+        // 渲染用户头像
+        if (user.user_pic !== null) {
+            $('.layui-nav-img').attr('src', user.user_pic).show();
+            $('.text-avatar').hide()
+        } else {
+            $('.layui-nav-img').hide();
+            var firstNum = name[0].toUpperCase();
+            $('.text-avatar').html(firstNum).show();
+        }
+    }
+})
